852973047f27e35890c80279a0594581ccc9c84e
yongming <zhuangyongming1900@gmail.com>
2018-04-14 00:42:22 +0800

    [#8][yongming,Junstin] update UserPage test case

--- client/src/components/UserPage/UserPage.test.js
+++ client/src/components/UserPage/UserPage.test.js
@@ -6,7 +6,7 @@ import { NavLink, Switch, Route } from "react-router-dom";
 describe("userPage structure Testing", () => {
   it("should find all structure element", () => {
     const wrapper = shallow(<UserPage />);
-    expect(wrapper.find("div")).toHaveLength(3);
+    expect(wrapper.find("div")).toHaveLength(2);
     expect(wrapper.find("li")).toHaveLength(4);
     expect(wrapper.find(NavLink)).toHaveLength(4);
     expect(wrapper.find(Switch)).toHaveLength(1);
