56dbb24c401d303f1f28c7fdb8795d0ccf105969
yongming <zhuangyongming1900@gmail.com>
2018-04-13 11:33:24 +0800

    [#8][yongming, jan] create ClientRouter File and refactor routing code

---
+++ client/src/components/ClientRouter/ClientRouter.js
@@ -0,0 +1,48 @@
+import React from "react";
+import Home from "../Home";
+import NotFound from "../NotFound";
+import Login from "../Account/Login";
+import Signup from "../Account/Signup";
+import Profile from "../Account/Profile";
+import Forgot from "../Account/Forgot";
+import Reset from "../Account/Reset";
+import { subscribe } from "react-contextual";
+import { Route, Switch, Redirect } from "react-router-dom";
+import userPage from "../userPage/userPage";
+
+const isAuthenticated = props => props.jwtToken !== null;
+
+const PrivateRoute = subscribe()(({ component: Component, ...rest }) => (
+  <Route
+    {...rest}
+    render={props =>
+      isAuthenticated(props) ? (
+        <Component {...props} />
+      ) : (
+        <Redirect
+          to={{
+            pathname: "/login",
+            state: { from: props.location }
+          }}
+        />
+      )
+    }
+  />
+));
+const ClientRouter = () => {
+  return (
+    <div>
+      <Switch>
+        <Route path="/" exact component={Home} />
+        <Route path="/login" component={Login} />
+        <Route path="/signup" component={Signup} />
+        <PrivateRoute path="/account" component={Profile} />
+        <Route path="/forgot" component={Forgot} />
+        <Route path="/reset/:token" component={Reset} />
+        <Route path="/userPage" component={userPage} />
+        <Route path="*" component={NotFound} />
+      </Switch>
+    </div>
+  );
+};
+export default ClientRouter;
