6a8c0a93fe43d332f4d5e854d66f3e6319067aa4
yongming <zhuangyongming1900@gmail.com>
2018-04-17 10:25:01 +0800

    [#8][yongming,justin] add states, onChangehandler and sharHandler

--- client/src/components/TemplateForm/TemplateForm.js
+++ client/src/components/TemplateForm/TemplateForm.js
@@ -1,16 +1,36 @@
 import React from "react";
 import { Prompt } from "react-router";
 import "./TemplateForm.css";
-// import { withRouter } from "react-router-dom";
+import { ProviderContext, subscribe } from "react-contextual";
+import {
+  mapMessageContextToProps,
+  mapSessionContextToProps,
+  messageContextPropType,
+  sessionContextPropType
+} from "../context_helper";
+import { object, instanceOf } from "prop-types";
+import { withCookies, Cookies } from "react-cookie";
+import { Share } from "../../actions/formUtils";
 
 class TemplateForm extends React.Component {
   constructor() {
     super();
     this.state = {
-      isSaved: false
+      isSaved: false,
+      email: "",
+      feedbackItem1: "",
+      feedbackItem2: "",
+      feedbackItem3: ""
     };
     this.shareHandler = this.shareHandler.bind(this);
   }
+  static propTypes = {
+    history: object.isRequired,
+    cookies: instanceOf(Cookies).isRequired,
+    ...messageContextPropType,
+    ...sessionContextPropType
+  };
+
   render() {
     return (
       <div className="content">
@@ -33,7 +53,12 @@ class TemplateForm extends React.Component {
             <div>
               <label htmlFor="email">Receiver's email address:</label>
             </div>
-            <textarea className="form-control border border-primary" rows={1} />
+            <textarea
+              className="form-control border border-primary"
+              rows={1}
+              value={this.state.email}
+              onChange={this.onChangeHandler.bind(this)}
+            />
             <br />
             <div>
               <label>You are doing great at...</label>
@@ -43,17 +68,20 @@ class TemplateForm extends React.Component {
               <textarea
                 className="form-control border border-primary"
                 rows={6}
+                value={this.state.feedbackItem1}
+                onChange={this.onChangeHandler.bind(this)}
               />
             </div>
             <br />
             <div>
               <label>You could work on/improve...</label>
             </div>
-
             <div className="feedback-form-fields">
               <textarea
                 className="form-control border border-primary"
                 rows={6}
+                value={this.state.feedbackItem2}
+                onChange={this.onChangeHandler.bind(this)}
               />
             </div>
             <br />
@@ -64,6 +92,8 @@ class TemplateForm extends React.Component {
               <textarea
                 className="form-control border border-primary"
                 rows={6}
+                value={this.state.feedbackItem3}
+                onChange={this.onChangeHandler.bind(this)}
               />
             </div>
           </form>
@@ -72,10 +102,37 @@ class TemplateForm extends React.Component {
     );
   }
 
+  onChangeHandler(event) {
+    event.preventDefault();
+    this.setState({
+      email: event.target.value,
+      feedbackItem1: event.target.value,
+      feedbackItem2: event.target.value,
+      feedbackItem3: event.target.value
+    });
+  }
+
   shareHandler(event) {
     event.preventDefault();
     this.setState({ isSaved: true });
+    Share({
+      email: this.state.email,
+      feedbackItem1: this.state.feedbackItem1,
+      feedbackItem2: this.state.feedbackItem2,
+      feedbackItem3: this.state.feedbackItem3,
+      history: this.props.history,
+      messageContext: this.props.messageContext
+    });
   }
 }
 
-export default TemplateForm;
+const mapContextToProps = context => {
+  return {
+    ...mapSessionContextToProps(context),
+    ...mapMessageContextToProps(context)
+  };
+};
+
+export default subscribe(ProviderContext, mapContextToProps)(
+  withCookies(TemplateForm)
+);
