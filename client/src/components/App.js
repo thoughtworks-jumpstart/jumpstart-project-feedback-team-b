a4ead108e6d381b3b7cc4a867a2f2f55db6d050d
songguoqiang <songguoq@gmail.com>
2018-04-18 14:49:38 +0800

    [#46][gordon] introduced new state 'isAuthenticated' in App.js so that it's updated synchronously during user login/logout

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,8 +1,10 @@
 import React from "react";
+import { BrowserRouter, Route, Switch, Redirect } from "react-router-dom";
 import { CookiesProvider } from "react-cookie";
+import { Provider } from "react-contextual";
+
 import Header from "./Header";
 import Footer from "./Footer";
-import { Provider } from "react-contextual";
 import Home from "./Home";
 import NotFound from "./NotFound";
 import Login from "./Account/Login";
@@ -11,47 +13,54 @@ import Profile from "./Account/Profile";
 import Forgot from "./Account/Forgot";
 import Reset from "./Account/Reset";
 import MyDashboard from "./MyDashboard/MyDashboard";
-import { subscribe } from "react-contextual";
-import { BrowserRouter, Route, Switch, Redirect } from "react-router-dom";
-
-const isAuthenticated = props => props.jwtToken !== null;
-
-const PrivateRoute = subscribe()(({ component: Component, ...rest }) => (
-  <Route
-    {...rest}
-    render={props =>
-      isAuthenticated(props) ? (
-        <Component {...props} />
-      ) : (
-        <Redirect
-          to={{
-            pathname: "/login",
-            state: { from: props.location }
-          }}
-        />
-      )
-    }
-  />
-));
-
-const store = {
-  initialState: { jwtToken: null, user: {}, messages: {} },
-  actions: {
-    saveSession: (jwtToken, user) => ({ jwtToken, user }),
-    clearSession: () => ({ jwtToken: null, user: {} }),
-    updateUserProfile: newProfile => state => ({
-      user: Object.assign(state.user, newProfile)
-    }),
-    clearMessages: () => ({ messages: {} }),
-    setErrorMessages: errors => ({ messages: { error: errors } }),
-    setSuccessMessages: success => ({ messages: { success: success } })
-  }
-};
 
 class App extends React.Component {
+  isAuthenticated = false;
+  saveSession = (jwtToken, user) => {
+    this.isAuthenticated = true;
+    return { jwtToken, user };
+  };
+
+  clearSession = () => {
+    this.isAuthenticated = false;
+    return { jwtToken: null, user: {} };
+  };
+
+  store = {
+    initialState: { jwtToken: null, user: {}, messages: {} },
+    actions: {
+      saveSession: this.saveSession,
+      clearSession: this.clearSession,
+      updateUserProfile: newProfile => state => ({
+        user: Object.assign(state.user, newProfile)
+      }),
+      clearMessages: () => ({ messages: {} }),
+      setErrorMessages: errors => ({ messages: { error: errors } }),
+      setSuccessMessages: success => ({ messages: { success: success } })
+    }
+  };
+
+  PrivateRoute = ({ component: Component, ...rest }) => (
+    <Route
+      {...rest}
+      render={props =>
+        this.isAuthenticated ? (
+          <Component {...props} />
+        ) : (
+          <Redirect
+            to={{
+              pathname: "/login",
+              state: { from: props.location }
+            }}
+          />
+        )
+      }
+    />
+  );
+
   render() {
     return (
-      <Provider {...store}>
+      <Provider {...this.store}>
         <CookiesProvider>
           <BrowserRouter>
             <div>
@@ -60,10 +69,13 @@ class App extends React.Component {
                 <Route path="/" exact component={Home} />
                 <Route path="/login" component={Login} />
                 <Route path="/signup" component={Signup} />
-                <PrivateRoute path="/account" component={Profile} />
+                <this.PrivateRoute path="/account" component={Profile} />
                 <Route path="/forgot" component={Forgot} />
                 <Route path="/reset/:token" component={Reset} />
-                <PrivateRoute path="/mydashboard" component={MyDashboard} />
+                <this.PrivateRoute
+                  path="/mydashboard"
+                  component={MyDashboard}
+                />
                 <Route path="*" component={NotFound} />
               </Switch>
               <div>
