956d1baab7ed472e4e4abcf939906834546ae27f
yongming <zhuangyongming1900@gmail.com>
2018-04-19 13:56:19 +0800

    [24#][ym,Faisal] add feedback component and update inbox component

--- client/src/components/Inbox/Feedback.js
+++ client/src/components/Inbox/Feedback.js
@@ -1,83 +1,98 @@
 import React from "react";
-import {
-  ProviderContext,
-  subscribe
-} from "react-contextual/dist/react-contextual";
 
-const Feedback = props => {
-  let id = props.match.params.id;
-  fetch("/api/feedback/" + id, {
-    method: "get",
-    headers: { "Content-Type": "application/json" }
-  })
-    .then(response => {
-      if (response.ok) {
-        return (
-          <div>
-            <div className="template-header">
-              <h3>
-                <strong>Feedback</strong>
-              </h3>
-            </div>
-            <form>
-              <div>
-                <label htmlFor="email">Receiver's email address:</label>
-              </div>
-              <textarea
-                className="form-control border border-primary"
-                rows={1}
-                name="email"
-                value={this.state.email}
-                disabled
-              />
-              <br />
-              <div>
-                <label>You are doing great at...</label>
-              </div>
-
-              <div className="feedback-form-fields">
-                <textarea
-                  className="form-control border border-primary"
-                  rows={6}
-                  name="feedbackItem1"
-                  value={response.body.feedback.feedbackItems[0]}
-                  disabled
-                />
-              </div>
-              <br />
-              <div>
-                <label>You could work on/improve...</label>
-              </div>
-              <div className="feedback-form-fields">
-                <textarea
-                  className="form-control border border-primary"
-                  rows={6}
-                  name="feedbackItem2"
-                  value={response.body.feedback.feedbackItems[1]}
-                  disabled
-                />
-              </div>
-              <br />
-              <div>
-                <label>Suggestions...</label>
-              </div>
-              <div className="feedback-form-fields">
-                <textarea
-                  className="form-control border border-primary"
-                  rows={6}
-                  value={response.body.feedback.feedbackItems[2]}
-                  name="feedbackItem3"
-                  disabled
-                />
-              </div>
-            </form>
-          </div>
-        );
+class Feedback extends React.Component {
+  // let id = props.match.params.id;
+  constructor() {
+    super();
+    this.state = {
+      feedback: { feedbackItems: [] }
+    };
+  }
+  fetchCall() {
+    fetch("/api/feedback/5ad70e4916492a09c0b96fa2", {
+      method: "get",
+      headers: {
+        "Content-Type": "application/json"
       }
     })
-    .catch(err => {
-      return <div>Invalid Page</div>;
-    });
-};
+      .then(response => {
+        response.json().then(json => {
+          this.setState({
+            feedback: json.feedback
+          });
+        });
+      })
+      .catch(error => {
+        throw new Error(error);
+      });
+  }
+  async componentDidMount() {
+    await this.fetchCall();
+  }
 
-export default subscribe(ProviderContext)(Feedback);
+  render() {
+    return (
+      <div>
+        <div className="template-header">
+          <h3>
+            <strong>Feedback</strong>
+          </h3>
+        </div>
+        <form>
+          <div>
+            <label htmlFor="email">Receiver's email address:</label>
+          </div>
+          <textarea
+            className="form-control border border-primary"
+            rows={1}
+            name="email"
+            value={this.state.feedback.giver}
+            disabled
+          />
+          <br />
+          <div>
+            <label>You are doing great at...</label>
+          </div>
+
+          <div className="feedback-form-fields">
+            <textarea
+              className="form-control border border-primary"
+              rows={6}
+              name="feedbackItem1"
+              value={this.state.feedback.feedbackItems[0]}
+              disabled
+            />
+          </div>
+          <br />
+          <div>
+            <label>You could work on/improve...</label>
+          </div>
+          <div className="feedback-form-fields">
+            <textarea
+              className="form-control border border-primary"
+              rows={6}
+              name="feedbackItem2"
+              value={this.state.feedback.feedbackItems[1]}
+              disabled
+            />
+          </div>
+          <br />
+          <div>
+            <label>Suggestions...</label>
+          </div>
+          <div className="feedback-form-fields">
+            <textarea
+              className="form-control border border-primary"
+              rows={6}
+              value={this.state.feedback.feedbackItems[2]}
+              name="feedbackItem3"
+              disabled
+            />
+          </div>
+        </form>
+      </div>
+    );
+  }
+}
+
+export default Feedback;
--- client/src/components/Inbox/Inbox.js
+++ client/src/components/Inbox/Inbox.js
@@ -4,6 +4,8 @@ import Feedback from "./Feedback";
 
 const Inbox = () => (
   <div>
+    <h1>hello</h1>
+    <Feedback />
     <Route path="/mydashboard/inbox/feedback/:id" exact component={Feedback} />
   </div>
 );
