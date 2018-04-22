e60cb7cb954b1fbbb9b3f77dc1718ec69ecf78a1
yappps <yapyuanxin@gmail.com>
2018-04-22 16:31:44 +0800

    Added a test case in FeedbackItemTemplate.test.js

--- client/src/components/FeedbackTemplate/FeedbackItemTemplate.test.js
+++ client/src/components/FeedbackTemplate/FeedbackItemTemplate.test.js
@@ -12,9 +12,10 @@ describe("FeedbackItemTemplate", () => {
         feedbackValue={sampleFeedbackValue}
       />
     );
-    expect(wrapper.find("textarea").props().value).toEqual(sampleFeedbackValue);
     expect(wrapper.find("label")).toHaveLength(1);
+    expect(wrapper.find("label").props().children).toEqual(sampleLabel);
     expect(wrapper.find("textarea")).toHaveLength(1);
+    expect(wrapper.find("textarea").props().value).toEqual(sampleFeedbackValue);
   });
   it("onChangehandler should be invoked when there is a change in the text field", () => {
     const mockOnChangeHandler = jest.fn();
@@ -32,7 +33,7 @@ describe("FeedbackItemTemplate", () => {
       .props()
       .onChange(event);
 
-    // expect(wrapper.props().feedbackValue).toEqual(event.target.value);
+    // expect(wrapper.find("textarea").props().feedbackValue).toEqual(event.target.value);
     expect(mockOnChangeHandler).toHaveBeenCalledTimes(1);
   });
 });
